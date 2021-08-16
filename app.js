941f9a8e4efcfe9b15301ded974258c2faaa33b5
midxdle <sina.heydarian.sh@gmail.com>
2021-08-16 18:52:29 +0430

    this is the engine of my app that will connect and run everything where needed

---
+++ app.js
@@ -0,0 +1,42 @@
+//import all necessary modules
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+require('./config/passport')(passport);
+
+//connect to mongodb and set express template
+let app = express();
+const PORT = process.env.PORT||3000;
+dotenv.config({ path: './config/config.env'});
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+//initialize middleware and setup database for storing sessions
+app.use(express.urlencoded({extended:true}));
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection}),
+    })
+);
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//last import routes
+app.use(require("./routes/index"));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(PORT, console.log(`listening at ${PORT}`));
