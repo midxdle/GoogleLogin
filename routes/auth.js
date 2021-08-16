d8dff942eed3f73676efd512366604c2b2933589
midxdle <sina.heydarian.sh@gmail.com>
2021-08-16 22:14:51 +0430

    user athenticate added for login and logout and auth user

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+//importing required modules
+const express = reuqire('express');
+const passport = require('passport');
+const router = express.Router();
+//send to google do the authentication
+//profile gets us their basic info(name) and email gets emails
+router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+
+//callback after google has authenticated the user
+router.get(
+    '/google/callback',
+    passport.authenticate('google', {failureRedirect: '/'}),
+    (req, res)=>{res.redirect('/log')}
+);
+
+//logout
+router.get('logout', (req, res)=> {
+    req.logout();
+    res.redirect('/');
+})
+
+module.exports = router;
