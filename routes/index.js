76fd5d1a58225bc992b9f29f4f82f09374fe4fec
midxdle <sina.heydarian.sh@gmail.com>
2021-08-16 22:36:04 +0430

    redirecting users after cheking auth for login or after log

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const router = require('express').router()
+
+//importing middleware
+const {ensureAuth, ensureGuest} = require('../middleware/auth');
+
+//redirect ensured guest to login page
+router.get('/', ensureGuest, (req, res)=> {
+    res.render('login')
+});
+
+//redirect ensured auth to index page
+router.get("/log", ensureAuth, async(req, res)=>{
+    res.render('index', {userinfo:req.user})
+});
+
+module.exports = router;
