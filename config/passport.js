dc81e5b1dbc266b6d23bf5de23f7263e0ccfb7c3
midxdle <sina.heydarian.sh@gmail.com>
2021-08-17 18:04:07 +0430

    due that .env file was not working some modules updated adn .env file location has been changed

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,9 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 module.exports = function(passport) {
     passport.use(
