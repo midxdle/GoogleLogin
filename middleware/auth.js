7635ffd55c39e952f9d67c11a4e0cefccc6ac871
midxdle <sina.heydarian.sh@gmail.com>
2021-08-17 19:04:52 +0430

    i just fogot a '!' at line 18 and now it fixed

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = {
     //if user is auth then redirect to the next page
     //else redirect to login page
     ensureAuth: function (req, res, next) {
-        if (req.isAuthenticated()) {
+        if(req.isAuthenticated()) {
             return next()
         } else {
             res.redirect('/')
@@ -15,7 +15,7 @@ module.exports = {
     //then redirect to home page
     //if not auth redirect to login page
     ensureGuest: function(req, res, next) {
-        if(req.isAuthenticated()) {
+        if(!req.isAuthenticated()) {
             return next();
         } else {
             res.redirect('/log');
